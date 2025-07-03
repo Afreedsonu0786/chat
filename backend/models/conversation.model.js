930e11815f1c67897e8577b7796b1f597c344cd3
Afreedsonu0786 <afreedsonu0786@gmail.com>
2025-07-02 20:44:24 -0500

    recover

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -1,22 +1,28 @@
 import mongoose from "mongoose";
-
-const conversationSchema = new mongoose.Schema(
+const messageSchema = new mongoose.Schema(
   {
-    participants: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    messages: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Message",
-      },
-    ],
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      default: "",
+    },
+    image: {
+      type: String,
+      default: "",
+    },
   },
   { timestamps: true }
 );
 
-const Conversation = mongoose.model("Conversation", conversationSchema);
-export default Conversation;
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
