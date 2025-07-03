5054dbe8a20064d3e5db16b5e6b4bc1bdad433b2
Afreedsonu0786 <afreedsonu0786@gmail.com>
2025-07-02 20:36:22 -0500

    .

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import connectDb from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRouter from "./routes/user.routes.js";
 import messageRouter from "./routes/message.routes.js";
-
-dotenv.config();
 
 const port = process.env.PORT || 5000;
 
