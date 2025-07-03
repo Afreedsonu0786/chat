35b7ecb7ce7616ea17badbedf01817f81287ae3a
Afreedsonu0786 <afreedsonu0786@gmail.com>
2025-07-02 20:52:03 -0500

    created messages and converstaion model and controllers and routes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import connectDb from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRouter from "./routes/user.routes.js";
-
-dotenv.config();
+import messageRouter from "./routes/message.routes.js";
 
 const port = process.env.PORT || 5000;
 
@@ -22,6 +22,7 @@ app.use(cookieParser());
 
 app.use("/api/v1", authRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/message", messageRouter);
 
 app.listen(port, () => {
   connectDb();
